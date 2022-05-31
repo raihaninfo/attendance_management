80c5a70c6086486f6df81996dcb6a2d977591793
raihaninfo <raihanmahmudi35@gmail.com>
2022-05-31 12:00:45 +0600

    create model

--- models/model.go
+++ models/model.go
@@ -1,1 +1,26 @@
 package models
+
+type User struct {
+	Id       int `gorm:"primaryKey"`
+	Name     string
+	UserName string
+	Email    string
+	Mobile   string
+	Password string
+	UserType int
+	Status   int
+}
+
+type Student struct {
+	Id     int `gorm:"primaryKey"`
+	Name   string
+	Roll   string
+	Class  string
+	Status int
+}
+
+type Class struct {
+	Id           int `gorm:"primaryKey"`
+	ClassName    string
+	ClassTeacher User
+}
