5ebe1b1241585302aab61af2b0a66d67173af4f0
raihaninfo <raihanmahmudi35@gmail.com>
2022-06-07 22:48:04 +0600

    create teachers handler

--- handlers/frontendHandler.go
+++ handlers/frontendHandler.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +14,37 @@ func (h handler) FrontHome(c *gin.Context) {
 	c.HTML(http.StatusOK, "home.gohtml", nil)
 }
 
+type Abc struct {
+	Id          int
+	Name        string
+	UserName    string
+	Email       string
+	Mobile      string
+	DateOfBirth string
+	Designation string
+	Password    string
+	UserType    int
+	Status      int
+}
+
+var abc Abc
+
 func (h handler) FrontTeacher(c *gin.Context) {
-	c.HTML(http.StatusOK, "teachers.gohtml", nil)
+	req, err := http.Get("https://jsonplaceholder.typicode.com/users")
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	json.Unmarshal(body, &abc)
+	fmt.Println(abc)
+	c.HTML(http.StatusOK, "teachers.gohtml", gin.H{
+		"Name":  abc.Name,
+		"Email": "raihanmahmudi35@gmail.com",
+	})
 }
